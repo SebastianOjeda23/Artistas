87cfbcba664e071bf82fe76d6a15e8be8292511b
SebastianOjeda23 <169169884+SebastianOjeda23@users.noreply.github.com>
2024-10-25 17:55:53 -0300

    diseño final
    se agrego lo ultimo del diseño y alguna validacion

--- ArtistaApp/forms.py
+++ ArtistaApp/forms.py
@@ -5,3 +5,16 @@ class FormArtista(forms.ModelForm):
     class Meta:
         model = Artista
         fields = '__all__'
+
+
+    def clean_cancionescreada(self):
+        cancionescreada = self.cleaned_data.get('cancionescreada')
+        if cancionescreada < 0:
+            raise forms.ValidationError("El número de canciones creadas no puede ser negativo.")
+        return cancionescreada
+    
+    def clean_aniosactivo(self):
+        aniosactivo = self.cleaned_data.get('aniosactivo')
+        if aniosactivo < 0:
+            raise forms.ValidationError("El número de años activo no puede ser negativo.")
+        return aniosactivo
