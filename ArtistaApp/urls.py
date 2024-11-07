db4f21aa89697f5cdcb152156c6726b53f2958c7
SebastianOjeda23 <169169884+SebastianOjeda23@users.noreply.github.com>
2024-11-07 20:38:20 -0300

    eva03
    commit para la eva 3 de backend con mayoria de cosas implementadas

--- ArtistaApp/urls.py
+++ ArtistaApp/urls.py
@@ -8,5 +8,13 @@ urlpatterns = [
     path('lista/',views.lista),
     path('agregar/',views.registrarArtista),
     path('eliminarArtista/<int:id>',views.eliminarArtista),
-    path('actualizarArtista/<int:id>',views.actualizarArtista)
+    path('actualizarArtista/<int:id>',views.actualizarArtista),
+    path('lista2/',views.listaMusica),
+    path('agregaMusica',views.registrarMusica),
+    path('actualizaMusica/<int:id>',views.actualizarMusica),
+    path('eliminaMusica/<int:id>',views.eliminaMusica),
+    path('lista3/',views.listaAlbum),
+    path('agregaAlbum/',views.registrarAlbum),
+    path('actualizaAlbum/<int:id>',views.actualizaAlbum),
+    path('eliminaAlbum/<int:id>',views.eliminarAlbum)
 ]
--- ArtistaApp/views.py
+++ ArtistaApp/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render,redirect
-from ArtistaApp.models import Artista
-from ArtistaApp.forms import FormArtista
+from ArtistaApp.models import Artista,Musica,   Album
+from ArtistaApp.forms import FormArtista,FormMusica,FormAlbum
 
 def index(request):
     return render(request,'ArtistaApp/index.html')
+
+def listaAlbum(request):
+    albumes = Album.objects.all()
+    data = {'albumes': albumes}
+    return render(request, 'ArtistaApp/listaAlbum.html', data)
+def registrarAlbum(request):
+    form2 = FormAlbum()
+    if request.method == 'POST':
+        form2 = FormAlbum(request.POST)
+        if form2.is_valid():
+            form2.save()
+            return redirect('../')
+        else:
+            data = {'form2':form2}
+            return render(request,'ArtistaApp/agregaAlbum.html',data)
+    else:
+        data = {'form2':form2}
+        return render(request,'ArtistaApp/agregaAlbum.html',data)
+    
+def actualizaAlbum(request,id):
+    albumes = Album.objects.get(id = id)
+    form2 = FormAlbum(instance=albumes)
+    if request.method == 'POST':
+        form2 = FormAlbum(request.POST,instance=albumes)
+        if form2.is_valid():
+            form2.save()
+        return index(request)
+    data = {'form2' : form2}
+    return render(request,'ArtistaApp/agregaAlbum.html',data)
+
+def eliminarAlbum(request,id):
+    albumes = Album.objects.get(id = id)
+    albumes.delete()
+    return redirect('../lista3')
 
 def lista(request):
     artistas = Artista.objects.all()
@@ -40,4 +74,42 @@ def eliminarArtista(request,id):
     artistas = Artista.objects.get(id = id)
     artistas.delete()
     return redirect('../lista')
+
+
+def listaMusica(request):
+    musicas = Musica.objects.all() 
+    data = {'musicas': musicas} 
+    return render(request, 'ArtistaApp/Musicalista.html', data)
+
+def registrarMusica(request):
+    form1 = FormMusica()
+    if request.method == 'POST':
+        form1 = FormMusica(request.POST)
+        if form1.is_valid():
+            form1.save()
+            return redirect('../')
+        else:
+            data = {'form1' : form1}
+            return render(request,'ArtistaApp/registraMusica.html',data)
+    else:
+        data = {'form1' : form1}
+        return render(request,'ArtistaApp/registraMusica.html',data)
+    
+def actualizarMusica(request,id):
+    musica = Musica.objects.get(id = id)
+    form1 = FormMusica (instance=musica)
+    if request.method == 'POST':
+        form1 = FormMusica(request.POST , instance=musica)
+        if form1.is_valid():
+            form1.save()
+        return index(request)
+    data = {'form1' : form1}
+    return render(request,'ArtistaApp/registraMusica.html',data)
+
+def eliminaMusica(request,id):
+    musica = Musica.objects.get(id = id)
+    musica.delete()
+    return redirect('../lista2')
+
+        
 # Create your views here.
